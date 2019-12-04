f77d2f1f7736c5cd83c7a4f3d992f754c74b5e07
zhd <2646405301@qq.com>
2019-12-04 17:17:37 +0800

    Site updated: 2019-12-04 17:17:34

--- assets/waifu-tips.js
+++ assets/waifu-tips.js
@@ -193,8 +193,8 @@ function initModel(waifuPath){
         
         /* 首次访问加载 指定模型 的 指定材质 */
         
-        var modelId = 4;            // 模型 ID
-        var modelTexturesId = 53    // 材质 ID
+        var modelId = 6;            // 模型 ID
+        var modelTexturesId = 3;    // 材质 ID
         
     } loadModel(modelId, modelTexturesId);
 	
